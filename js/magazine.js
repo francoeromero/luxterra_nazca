1d3542a8ffe29cb541ea8648c8777e9442fbfdb3
Fran Romero <franromeroeze@gmail.com>
2025-03-06 20:02:17 -0300

    todo

--- js/magazine.js
+++ js/magazine.js
@@ -434,19 +434,19 @@ function addPage(page, book) {
 		lista_webp = [
 			'./webp/pc/0.webp',
 			'./webp/pc/1.webp',
-			'./webp/pc/2.webp',
+			'./webp/pc/5.webp',
 			'./webp/pc/3.webp',
 			'./webp/pc/4.webp',
-			'./webp/pc/5.webp',
+			'./webp/pc/2.webp',
 			'./webp/pc/6.webp',
 		];
 		lista_webp_movil = [
 			'./webp/pc/0.webp',
 			'./webp/pc/1.webp',
-			'./webp/pc/2.webp',
+			'./webp/pc/5.webp',
 			'./webp/pc/3.webp',
 			'./webp/pc/4.webp',
-			'./webp/pc/5.webp',
+			'./webp/pc/2.webp',
 			'./webp/pc/6.webp',
 		];
 
